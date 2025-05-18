220e570ce00ae8285476aa7160fa8a1c17fbc41b
Moezhie <muziburrahman.st@gmail.com>
2025-05-19 02:47:50 +0700

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,5 +1,5 @@
 const TELEGRAM_BOT_TOKEN = "7872065476:AAGZL0Dpb-Ni6j2MWoZFQwryqRUl7Catvsk";
-const TELEGRAM_CHAT_ID = "Investasicerdasbot"
+const TELEGRAM_CHAT_ID = "@Investasicerdasbot"
 
 async function sendTelegramMessage(message) {
     const url = `https://api.telegram.org/bot${"7872065476:AAGZL0Dpb-Ni6j2MWoZFQwryqRUl7Catvsk"}/sendMessage`;
@@ -8,7 +8,7 @@ async function sendTelegramMessage(message) {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            chat_id: "Investasicerdasbot",
+            chat_id: "@Investasicerdasbot",
             text: message
         })
     });
