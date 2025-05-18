79c4c4567e10e55ad889ba494d27d1bc5a8e3507
Moezhie <muziburrahman.st@gmail.com>
2025-05-19 02:49:05 +0700

    Update telegram_menu.js

--- telegram_menu.js
+++ telegram_menu.js
@@ -1,8 +1,8 @@
 async function sendTelegramMenu() {
-    const url = `https://api.telegram.org/bot${7872065476:AAGZL0Dpb-Ni6j2MWoZFQwryqRUl7Catvsk}/sendMessage`;
+    const url = `https://api.telegram.org/bot${"7872065476:AAGZL0Dpb-Ni6j2MWoZFQwryqRUl7Catvsk"}/sendMessage`;
 
     const messageData = {
-        chat_id: "Investasicerdasbot",
+        chat_id: "@Investasicerdasbot",
         text: "📊 Menu Investasi:\nPilih opsi di bawah untuk melihat data terkini:",
         reply_markup: {
             inline_keyboard: [
